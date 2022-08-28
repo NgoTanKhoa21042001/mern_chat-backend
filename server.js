63ccde2370e9bab33a39ee93ca310c7c0a1c6046
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-08-28 21:18:29 +0700

    Xử lí phần Socket

--- server.js
+++ server.js
@@ -23,6 +23,46 @@ const io = require("socket.io")(server, {
   },
 });
 
+// Rooms
+
+app.get("/rooms", (req, res) => {
+  res.json(rooms);
+});
+
+async function getLastMessagesFromRoom(room) {
+  let roomMessages = await Message.aggregate([
+    { $match: { to: room } },
+    { $group: { _id: "$date", messageByDate: { $push: "$$ROOT" } } },
+  ]);
+  return roomMessages;
+}
+
+function sortRoomMessagesByDate(messages) {
+  return messages.sort(function (a, b) {
+    let date1 = a._id.split("/");
+    let date2 = b._id.split("/");
+    date1 = date1[2] + date1[0] + date1[1];
+    date2 = date2[2] + date2[0] + date2[1];
+
+    return date1 < date2 ? -1 : 1;
+  });
+}
+
+// socket connection
+
+io.on("connection", (socket) => {
+  socket.on("new-user", async () => {
+    const members = await User.find();
+    io.emit("new-user", members);
+  });
+  socket.on("join room", async (room) => {
+    socket.join(room);
+    let roomMessages = await getLastMessagesFromRoom(room);
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    socket.emit("room-messages", roomMessages);
+  });
+});
+
 server.listen(PORT, () => {
   console.log("listening to port", PORT);
 });
